8bd79f46321ff082d83756f42ac63d4ab76e3474
Sportsguy <mahoward2s@gmail.com>
2021-12-26 19:50:27 -0600

    start of the coding game.  Going to work on sprites next

--- game.js
+++ game.js
@@ -3,9 +3,18 @@ kaboom({
     fullscreen: true,
     scale: 1,
     debug: true,
+    clearColor: [0, 0, 0, 1],
 })
 
+
+loadSprite()
+
+
 screen("game", () => {
+    layers(['bg', 'obj', 'ui'], 'obj')
+
+
+
 
 })
 

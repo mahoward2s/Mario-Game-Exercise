7e84435cf17bbdabc87541b5372e38e37037a4e3
Sportsguy <mahoward2s@gmail.com>
2021-12-29 09:41:25 -0600

    adding code to link for the sprites

--- game.js
+++ game.js
@@ -7,7 +7,15 @@ kaboom({
 })
 
 
-loadSprite()
+loadRoot('https://imgur.com/')
+loadSprite('coin', 'wbKxhcd')
+loadSprite('goomba', 'KPO3fR9')
+loadSprite('brick', 'pogC9x5')
+loadSprite('block', 'bdrLpi6')
+loadSprite('mario', 'Wb1qfhK')
+loadSprite('fireFlower', 'uaUm9sN')
+loadSprite('growMushroom', '0wMd92p')
+loadSprite('tunnel', 'rl3cTER')
 
 
 screen("game", () => {

7bb1f2a9f3a96a0b08b82e1633439588dad9ce83
Sportsguy <mahoward2s@gmail.com>
2021-12-29 10:36:56 -0600

    added additional sprites to the map and fixed a few typos in the code

--- game.js
+++ game.js
@@ -1,32 +1,59 @@
 kaboom({
     global: true,
     fullscreen: true,
-    scale: 1,
+    scale: 2,
     debug: true,
     clearColor: [0, 0, 0, 1],
 })
 
 
-loadRoot('https://imgur.com/')
-loadSprite('coin', 'wbKxhcd')
-loadSprite('goomba', 'KPO3fR9')
-loadSprite('brick', 'pogC9x5')
-loadSprite('block', 'bdrLpi6')
-loadSprite('mario', 'Wb1qfhK')
-loadSprite('fireFlower', 'uaUm9sN')
-loadSprite('mushroom', '0wMd92p')
-loadSprite('suprise', 'gesQ1KP')
-loadSprite('unboxed', 'bdrLpi6')
-loadSprite('pipeTopLeft', 'ReTPiWY')
-loadSprite('pipeTopRight', 'hj2GK4n')
-loadSprite('pipeBottomLeft', 'c1cYSbt')
-loadSprite('pipeBottomRight', 'nqQ79eI')
+loadRoot('https://i.imgur.com/')
 
-screen("game", () => {
+loadSprite('coin', 'wbKxhcd.png')
+loadSprite('goomba', 'KPO3fR9.png')
+loadSprite('brick', 'pogC9x5.png')
+loadSprite('block', 'M6rwarW.png')
+loadSprite('mario', 'Wb1qfhK.png')
+loadSprite('fireFlower', 'uaUm9sN.png')
+loadSprite('mushroom', '0wMd92p.png')
+loadSprite('surprise', 'gesQ1KP.png')
+loadSprite('unboxed', 'bdrLpi6.png')
+loadSprite('pipeTopLeft', 'ReTPiWY.png')
+loadSprite('pipeTopRight', 'hj2GK4n.png')
+loadSprite('pipeBottomLeft', 'c1cYSbt.png')
+loadSprite('pipeBottomRight', 'nqQ79eI.png')
+
+scene("game", () => {
     layers(['bg', 'obj', 'ui'], 'obj')
 
+    const map = [
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '      %   =*=%=                       ',
+        '                                      ',
+        '                            -+        ',
+        '                     ^  ^   ()        ',
+        '==============================   =====',
+    ]
 
+    const levelCfg = {
+        width: 20,
+        height: 20,
+        '=': [sprite('block'), solid()],
+        '$': [sprite('coin')],
+        '%': [sprite('surprise'), solid(), 'coinSurprise'],
+        '*': [sprite('surprise'), solid(), 'mushroomSurprise'],
+        '}': [sprite('unboxed'), solid()],
+        '(': [sprite('pipeBottomLeft'), solid()],
+        ')': [sprite('pipeBottomRight'), solid()],
+        '-': [sprite('pipeTopLeft'), solid()],
+        '+': [sprite('pipeTopRight'), solid()],
+    }
 
+    const gameLevel = addLevel(map, levelCfg)
 
 })
 
